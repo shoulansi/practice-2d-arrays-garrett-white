a9b8ae71ba2a680e6c889c2d0115792bca2523ed
shoulansi <garrettswhite@gmail.com>
2025-03-21 11:41:57 -0500

    final 2d array

--- practice-2d-arrays.js
+++ practice-2d-arrays.js
@@ -1,1 +1,16 @@
+let allScores = [
+    [100, 97, 81],
+    [87, 72, 90],
+    [94, 98, 95]
+];
 
+let theaterSeating = [
+    ["D1", "D2", "D3", "D4"],
+    ["E1", "E2", "E3", "E4"],
+    ["F1", "F2", "F3", "F4"]
+];
+
+let contactsList = [
+    ["Garrett", 22, "Rock Climber"],
+    ["Hadyn", 21, "Professional Magento Player"]
+];
